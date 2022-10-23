fa5e53967fc5b55ac1a41a0f22c9202602313ba4
Astika Luintel <astild2020@gmail.com>
2022-10-23 13:10:53 -0400

    commit tree fa393a9e8d01f1c60980eb358119759122d08b8f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, :json
+  # Content_type 'application/json'
   # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = Bakery.order(:price).all
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_goods = Bakery.order(:price).last
+    baked_goods.to_json
+  end
 
 end
